9e2cfde59f6e0b76b3ea4732d933b61373b8cdef
ijarvis <ijarvis@sina.com>
2020-05-31 15:32:26 +0800

    构建日常的开发环境，用vscode通过ssh连接进去美滋滋

---
+++ centos7_dev/start.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+test -f /etc/ssh/ssh_host_ecdsa_key || /usr/bin/ssh-keygen -q -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -C '' -N ''
+test -f /etc/ssh/ssh_host_rsa_key || /usr/bin/ssh-keygen -q -t rsa -f /etc/ssh/ssh_host_rsa_key -C '' -N ''
+test -f /etc/ssh/ssh_host_ed25519_key || /usr/bin/ssh-keygen -q -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -C '' -N ''
+test -f /root/.ssh/id_rsa || /usr/bin/ssh-keygen -t rsa -f /root/.ssh/id_rsa -N ''
+test -f /root/.ssh/id_rsa.pub || ssh-keygen -y -t rsa -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+test -f /root/.ssh/authorized_keys || /usr/bin/cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+
+
+if [[ $rootpass != "" ]];then
+    echo $rootpass | passwd root --stdin
+else
+    echo "liuwenru" | passwd root --stdin
+fi
+
+if [[ $sshport != "" ]];then
+    sed -i 's/#Port.*/Port '$sshport'/g' /etc/ssh/sshd_config
+else
+    sed -i 's/#Port.*/Port 2223/g' /etc/ssh/sshd_config
+fi
+
+/usr/sbin/sshd -D -e
